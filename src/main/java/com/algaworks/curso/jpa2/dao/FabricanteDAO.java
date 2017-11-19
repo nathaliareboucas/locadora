cc693b53d572c22bb2418ce2c242a7aa1d87f8e7
Nathalia <nathreboucas5@gmail.com>
2017-11-18 23:24:54 -0300

    Pesquisa, edição e exclusão fabricante

--- src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
+++ src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -1,11 +1,14 @@
 package com.algaworks.curso.jpa2.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.service.NegocioException;
+import com.algaworks.curso.jpa2.jpaUtil.Transactional;
 
 public class FabricanteDAO implements Serializable {
 
@@ -13,7 +16,24 @@ public class FabricanteDAO implements Serializable {
 	private EntityManager em;
 	
 	public void salvar(Fabricante fabricante) {
-		em.persist(fabricante);
+		em.merge(fabricante);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Fabricante> buscarTodos() {
+		return em.createQuery("from Fabricante").getResultList();
+	}
+
+	@Transactional
+	public void excluir(Fabricante fabricante) throws NegocioException {
+		Fabricante fabricanteTemp = em.find(Fabricante.class, fabricante.getCodigo());
+		
+		em.remove(fabricanteTemp);
+		em.flush();
+	}
+
+	public Fabricante buscarPeloCodigo(Long codigo) {
+		return em.find(Fabricante.class, codigo);
 	}
 	
 }
--- src/main/java/com/algaworks/curso/jpa2/service/NegocioException.java
+++ src/main/java/com/algaworks/curso/jpa2/service/NegocioException.java
@@ -2,6 +2,8 @@ package com.algaworks.curso.jpa2.service;
 
 public class NegocioException extends Exception {
 
+	private static final long serialVersionUID = 1L;
+
 	public NegocioException(String message) {
 		super(message);
 	}
