d4df1eae7c044e0ecd1ceaa3a030fe34bda22c37
Nathalia <nathreboucas5@gmail.com>
2017-11-25 23:12:10 -0300

    Data pedido, data entrega, data devolução do aluguel do carro

--- src/main/java/com/algaworks/curso/jpa2/modelo/Aluguel.java
+++ src/main/java/com/algaworks/curso/jpa2/modelo/Aluguel.java
@@ -1,6 +1,8 @@
 package com.algaworks.curso.jpa2.modelo;
 
 import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Aluguel {
@@ -17,41 +21,75 @@ public class Aluguel {
 	private Long codigo;
 	private BigDecimal valorTotal;
 	private Carro carro;
-	
+	private Calendar dataPedido;
+	private Date dataEntrega;
+	private Date dataDevolucao;
+
 	private ApoliceSeguro apoliceSeguro;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
-	
+
 	public BigDecimal getValorTotal() {
 		return valorTotal;
 	}
+
 	public void setValorTotal(BigDecimal valorTotal) {
 		this.valorTotal = valorTotal;
 	}
 
-	@OneToOne(cascade=CascadeType.ALL)
-	@JoinColumn(name="codigo_apolice_seguro")
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "codigo_apolice_seguro")
 	public ApoliceSeguro getApoliceSeguro() {
 		return apoliceSeguro;
 	}
+
 	public void setApoliceSeguro(ApoliceSeguro apoliceSeguro) {
 		this.apoliceSeguro = apoliceSeguro;
 	}
-	
+
 	@ManyToOne
-	@JoinColumn(name="codigo_carro")
+	@JoinColumn(name = "codigo_carro")
 	public Carro getCarro() {
 		return carro;
 	}
+
 	public void setCarro(Carro carro) {
 		this.carro = carro;
+	}
+
+	@Temporal(TemporalType.DATE)
+	public Calendar getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(Calendar dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(Date dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getDataDevolucao() {
+		return dataDevolucao;
+	}
+
+	public void setDataDevolucao(Date dataDevolucao) {
+		this.dataDevolucao = dataDevolucao;
 	}
 
 	@Override
@@ -61,7 +99,7 @@ public class Aluguel {
 		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -78,5 +116,5 @@ public class Aluguel {
 			return false;
 		return true;
 	}
-	
+
 }
--- src/main/java/com/algaworks/curso/jpa2/service/CadastroAluguelService.java
+++ src/main/java/com/algaworks/curso/jpa2/service/CadastroAluguelService.java
@@ -1,6 +1,7 @@
 package com.algaworks.curso.jpa2.service;
 
 import java.io.Serializable;
+import java.util.Calendar;
 
 import javax.inject.Inject;
 
@@ -11,17 +12,19 @@ import com.algaworks.curso.jpa2.jpaUtil.Transactional;
 public class CadastroAluguelService implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private AluguelDAO aluguelDAO;
-	
+
 	@Transactional
 	public void salvar(Aluguel aluguel) throws NegocioException {
-		
+
 		if (aluguel.getCarro() == null) {
 			throw new NegocioException("O carro é obrigatório");
 		}
-		
+
+		aluguel.setDataPedido(Calendar.getInstance());
+
 		this.aluguelDAO.salvar(aluguel);
 	}
 
